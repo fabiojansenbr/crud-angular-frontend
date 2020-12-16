68fe22d5652c4240a913038c41f9b424f51d31e3
Fábio Jansen <fabiojansen@outlook.com>
2020-12-16 11:01:08 -0300

    Gerenciando erros

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,35 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Product } from '../product.model';
-import { ProductService } from '../product.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { Product } from "../product.model";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-product-create',
-  templateUrl: './product-create.component.html',
-  styleUrls: ['./product-create.component.css']
+  selector: "app-product-create",
+  templateUrl: "./product-create.component.html",
+  styleUrls: ["./product-create.component.css"],
 })
 export class ProductCreateComponent implements OnInit {
-
   product: Product = {
-    name: '',
-    price: null
-  }
+    name: "",
+    price: null,
+  };
 
-  constructor(private productService: ProductService, private router: Router) { }
+  constructor(private productService: ProductService, private router: Router) {}
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   createProduct(): void {
     this.productService.create(this.product).subscribe(() => {
-      this.productService.showMessage('Produto criado com sucesso');
-      this.router.navigate(['/products']);
+      this.productService.showMessage("Produto criado com sucesso");
+      this.router.navigate(["/products"]);
     });
   }
 
   cancel(): void {
-    this.router.navigate(['/products']);
+    this.router.navigate(["/products"]);
   }
-
 }
--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Product } from '../product.model';
-import { ProductService } from '../product.service';
+import { Component, OnInit } from "@angular/core";
+import { Product } from "../product.model";
+import { ProductService } from "../product.service";
 
 @Component({
-  selector: 'app-product-read',
-  templateUrl: './product-read.component.html',
-  styleUrls: ['./product-read.component.css']
+  selector: "app-product-read",
+  templateUrl: "./product-read.component.html",
+  styleUrls: ["./product-read.component.css"],
 })
 export class ProductReadComponent implements OnInit {
-
   isLoading = true;
 
   products: Product[];
-  displayedColumns = ['id', 'name', 'price', 'actions'];
+  displayedColumns = ["id", "name", "price", "actions"];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.read().subscribe((products) => {
@@ -22,7 +21,6 @@ export class ProductReadComponent implements OnInit {
       this.isLoading = false;
 
       console.log(products);
-    })
+    });
   }
-
 }
